0cf06669759e55d3f16b57f375047cad3e98f297
Alex Taxiera <alex.taxiera@onesky.com>
2020-05-20 00:51:25 -0400

    add pre-query and mark deleted globals

--- packages/@alex-taxiera/poke-api/src/api/models/base.ts
+++ packages/@alex-taxiera/poke-api/src/api/models/base.ts
@@ -1,13 +1,29 @@
 import {
-  prop, modelOptions
+  prop, modelOptions, pre, DocumentType,
 } from '@typegoose/typegoose'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { FilterQuery } from 'mongoose'
 
 @modelOptions({
   schemaOptions: {
     timestamps: true,
     versionKey: false,
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
+  },
+})
+@pre<Base>([
+  'count',
+  'find',
+  'findOne',
+  'findOneAndRemove',
+  'findOneAndUpdate',
+  'update',
+  'updateOne',
+  'updateMany',
+], function () {
+  if (this.getQuery().isDeleted === undefined) {
+    this.where('isDeleted').equals(false)
   }
 })
 export class Base {
@@ -15,4 +31,16 @@ export class Base {
   @prop({ default: false })
   public isDeleted!: boolean
 
-}
+  public static async findOneAndMarkDeleted<T extends Base = Base> (
+    this: ModelType<any>,
+    filter: FilterQuery<any>,
+  ): Promise<DocumentType<T> | undefined> {
+    const model = await this.findOneAndUpdate({
+      ...filter,
+      isDeleted: false,
+    }, {
+      isDeleted: true,
+    }, { new: true })
+
+    return model ?? undefined
+  }
