73483eec567ee7b1ddb4fefa73ef0724327bfa0a
Alex Taxiera <alex.taxiera@onesky.com>
2020-05-25 11:42:56 -0400

    add config service

---
+++ packages/poke-ng-shared/projects/config-service/src/lib/config.service.ts
@@ -0,0 +1,96 @@
+import {
+  Injectable,
+  APP_INITIALIZER,
+  Provider,
+  InjectionToken,
+  Inject,
+  Optional,
+} from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+const defaultFilePath = 'assets/config/config.json'
+
+export const CONFIG_URL_TOKEN = new InjectionToken<string>('config path')
+
+/**
+ * A configuration service class that uses HttpClient to load configuration settings.
+ *
+ * To use it, first call the `load()` method to load the configuration.
+ * Then use the `get()` method to get a value for a specific key.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigService {
+
+  private settings?: { [key: string]: any }
+
+  constructor (
+    private readonly http: HttpClient,
+    @Optional()
+    @Inject(CONFIG_URL_TOKEN)
+    private readonly url?: string,
+  ) {}
+
+  /**
+   * Load the configuration settings using HttpClient get method.
+   *
+   * @returns A function that returns a Promise.
+   * The function is used by the factory method in defining `APP_INITIALIZER` provide.
+   */
+  public load (): () => Promise<void> {
+    return (): Promise<void> => {
+      return this.http
+        .get(this.url ?? defaultFilePath)
+        .toPromise()
+        .then((data) => {
+          this.settings = data
+        })
+        .catch((error) => {
+          throw new Error(
+            `ConfigService failed to load ${
+              this.url ?? defaultFilePath
+            }. Error: ${
+              error.message || (error.error ? error.error.message : 'undefined')
+            }`,
+          )
+        })
+    }
+  }
+
+  /**
+   * Get the configuration value for the specified key.
+   *
+   * @param key The key of the configuration value.
+   * @returns The value of the specified configuration key.
+   */
+  public get<T> (key: string): T | undefined {
+    let result: T | undefined
+
+    if (this.settings) {
+      result = this.settings[key]
+    }
+    return result
+  }
+
+}
+
+/**
+ * Called by a `APP_INITIALIZER` provider factory to load configuration and return a promise.
+ * The AOT requires a function, not an expression (arrow function or funciton expression)
+ * @ignore
+ */
+export function initializeService (config: ConfigService): () => Promise<void> {
+  return config.load()
+}
+
+/**
+ * Initialize the `APP_INITIALIZER` provider.
+ * It should be added to the `providers` property in AppModule definition.
+ */
+export const bootConfigServiceProvider: Provider = {
+  provide: APP_INITIALIZER,
+  useFactory: initializeService,
+  multi: true,
+  deps: [ ConfigService ],
+}
---
+++ packages/poke-ng-shared/projects/config-service/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of config-service
+ */
+
+export * from './lib/config.service'
