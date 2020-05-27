12eed96cdfc4cbdc3c2ef4c009d73601c7092d3d
Alex Taxiera <alex.taxiera@onesky.com>
2020-05-27 19:52:16 -0400

    add cors

--- packages/poke-api/src/index.ts
+++ packages/poke-api/src/index.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import fs from 'fs'
 
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import {
   Request,
   Response,
@@ -33,6 +34,7 @@ class NormalRouterServer extends Server {
 
   constructor () {
     super(true)
+    this.app.use(cors())
     this.app.use(bodyParser.json())
     this.app.use(bodyParser.urlencoded({ extended: true }))
     super.addControllers([
