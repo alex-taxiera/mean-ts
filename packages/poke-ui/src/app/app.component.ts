0649071fb00d4dc1f83a833d523fdb34584d907e
Alex Taxiera <alex.taxiera@onesky.com>
2020-05-27 19:54:39 -0400

    setup test config and api usage

--- packages/poke-ui/src/app/app.component.ts
+++ packages/poke-ui/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
 
+import { ConfigService } from '@poke-app/ng-config-service'
+// import { Schemas } from '@poke-app/api'
+import { TypeService } from '@poke-app/ng-api-services'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,5 +12,16 @@ import { Component } from '@angular/core'
 export class AppComponent {
 
   public title = 'poke-ui'
+  public readonly test?: string
+
+  constructor (
+    private readonly configService: ConfigService,
+    private readonly typeService: TypeService,
+  ) {
+    this.test = this.configService.get('apiUrl')
+    this.typeService.getAllTypes().subscribe((t) => {
+      console.log('t :>> ', t);
+    })
+  }
 
 }
--- packages/poke-ui/src/app/app.module.ts
+++ packages/poke-ui/src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { environment } from '../environments/environment'
+
+import {
+  bootConfigServiceProvider,
+  CONFIG_URL_TOKEN,
+  ConfigService,
+} from '@poke-app/ng-config-service'
+import {
+  SERVICE_URL_TOKEN,
+} from '@poke-app/ng-api-services'
+
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: SERVICE_URL_TOKEN,
+      useFactory: (configService: ConfigService): string =>
+        configService.get('apiUrl') ?? 'eff',
+      deps: [ ConfigService ],
+    },
+    {
+      provide: CONFIG_URL_TOKEN,
+      useValue: `assets/config/${
+        environment.production ? 'config' : 'config.local'
+      }.json`,
+    },
+    bootConfigServiceProvider,
+  ],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
